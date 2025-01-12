c91287d1954be8a038bd526230340b09ce206a0f
guanghaokwa <kwaguanghao@gmail.com>
2025-01-12 16:13:19 +0800

    Add PgAdmin4 Database

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import secrets
 SECRET_KEY = secrets.token_hex(24)
 NGROK_TOKEN = "YOUR_NGROK_TOKEN"
 DB_PARAMS = {
-    "database": "RecycleMgmt", 
-    "user": "postgres",
-    "password": "password",
-    "host": "localhost",
-    "port": "5432"
+    "database": "GreenMerlionAI", # Replace with your database name
+    "user": "postgres", # Replace with your postgresql username
+    "password": "password", # Replace with your postgresql password
+    "host": "localhost", # Replace with host name (Default: localhost)
+    "port": "5432" # Replacce with port number (Default: 5432)
 }
