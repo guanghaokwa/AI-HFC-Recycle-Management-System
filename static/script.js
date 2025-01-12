9b0f478bf5f6206ff7b253415f5e4e9bbba699f0
ZulfaqarHafez <zulfaqarhafez@gmail.com>
2025-01-12 14:48:16 +0800

    Styling for each pages & detection.html

--- static/script.js
+++ static/script.js
@@ -1,25 +1,68 @@
+const video = document.getElementById("live-video");
+const canvas = document.getElementById("detection-overlay");
+//const ctx = canvas.getContext("2d");
+const detectionResults = document.getElementById("detection-results");
+
+// Start video stream
+navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+        video.srcObject = stream;
+    })
+    .catch((err) => {
+        console.error("Error accessing webcam:", err);
+    });
+
+window.onload = () => {
+    let str_value = document.getElementById('form_info').value
+    console.log(str_value)
+    if (str_value == 'hide') {
+        document.getElementById('upload_video').style.display = "none"
+         document.getElementById('upload_form').style.display = "none"
+        document.getElementById('qrcode_container').style.display = "block"
+        document.getElementById('qrcode-info').style.display = "block"
+
+        setTimeout(() => {
+             document.getElementById('upload_video').style.display = "block"
+              document.getElementById('upload_form').style.display = "block"
+             document.getElementById('qrcode_container').style.display = "none"
+             document.getElementById('qrcode-info').style.display = "block"
+
+             document.getElementById('form_info').value = 'not yet'
+
+             window.location = '/'
+        }, 60000);
+    } else {
+        document.getElementById('upload_video').style.display = "block"
+         document.getElementById('upload_form').style.display = "block"
+        document.getElementById('qrcode_container').style.display = "none"
+        document.getElementById('qrcode-info').style.display = "block"
+    }
+    
+}
+
 function navigateTo(value) {
     if (value) {
         window.location.href = value;
     }
 }
 
-function sendMessage() {
-    const input = document.getElementById('chat-input');
-    const chatDisplay = document.getElementById('chat-display');
+// function sendMessage() {
+//     const input = document.getElementById('chat-input');
+//     const chatDisplay = document.getElementById('chat-display');
 
-    if (input.value.trim()) {
-        const userMessage = document.createElement('div');
-        userMessage.className = 'message user-message';
-        userMessage.textContent = input.value;
-        chatDisplay.appendChild(userMessage);
+//     if (input.value.trim()) {
+//         const userMessage = document.createElement('div');
+//         userMessage.className = 'message user-message';
+//         userMessage.textContent = input.value;
+//         chatDisplay.appendChild(userMessage);
 
-        const botMessage = document.createElement('div');
-        botMessage.className = 'message bot-message';
-        botMessage.textContent = "I'm sorry, I didn't understand that.";
-        chatDisplay.appendChild(botMessage);
+//         const botMessage = document.createElement('div');
+//         botMessage.className = 'message bot-message';
+//         botMessage.textContent = "I'm sorry, I didn't understand that.";
+//         chatDisplay.appendChild(botMessage);
 
-        input.value = '';
-        chatDisplay.scrollTop = chatDisplay.scrollHeight;
-    }
-}
+//         input.value = '';
+//         chatDisplay.scrollTop = chatDisplay.scrollHeight;
+//     }
+// }
